4b0c3d1449d663c7496eb247513159c8fb27277d
wangrich <wangxiayang.fdt@gmail.com>
2015-06-27 01:23:09 +0800

    add read/open

--- store.c
+++ store.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <errno.h>
 #include <dirent.h>
+#include <unistd.h>
 
 static char *rpath_prefix = "/home/xywang/fuse-store";
 
@@ -55,9 +56,48 @@ static int wfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
 	return 0;
 }
 
+static int wfs_open(const char *path, struct fuse_file_info *fi)
+{
+  	printf("open path=%s flag=%d\n", path, fi->flags);
+
+	char real_path[full_length(path) + 1];
+	strcpy(real_path, rpath_prefix);
+	strcpy(real_path + strlen(rpath_prefix), path);
+
+  	int fd = open(real_path, fi->flags);
+	if(fd < 0)
+	  	return -ENOENT;
+
+	fi->fh = fd;
+	return 0;
+}
+
+static int wfs_read(const char *path,
+	char *buf,
+	size_t size,
+	off_t offset,
+	struct fuse_file_info *fi)
+{
+	printf("read path=%s\n", path);
+
+	char real_path[full_length(path) + 1];
+	strcpy(real_path, rpath_prefix);
+	strcpy(real_path + strlen(rpath_prefix), path);
+
+  	uint64_t fd = fi->fh;
+	if (!fd) {
+	  	fprintf(stderr, "no file discriptor for path=%s\n", path);
+	  	return 0;
+	}
+
+	return read(fd, buf, size);
+}
+
 static struct fuse_operations wfs_oper = {
   	.getattr = wfs_getattr,
 	.readdir = wfs_readdir,
+	.open = wfs_open,
+	.read = wfs_read,
 };
 
 int main(int argc, char *argv[])
